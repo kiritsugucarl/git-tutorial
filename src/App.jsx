4e50df165cc204cae4b6cbebb3352b2b6767bc0d
Kiritsugucarl <bluesniper017@gmail.com>
2023-09-04 10:35:38 +0800

    add dev

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div>
       <h1>CAPSTONE</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit qui possimus unde doloremque obcaecati sapiente laborum omnis velit tenetur alias? Ex officiis maxime, doloremque dolorum repellendus quasi eveniet omnis quibusdam.</p>
+      <p>Dev branch activate</p>
     </div>
   )
 }
